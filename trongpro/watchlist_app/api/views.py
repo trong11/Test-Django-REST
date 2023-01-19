2788c929551a935252aa8263a6ac6f664c6788f6
trong11 <trongnp.nqd@gmail.com>
2023-01-19 14:15:22 +0700

    1

--- trongpro/watchlist_app/api/views.py
+++ trongpro/watchlist_app/api/views.py
@@ -29,12 +29,14 @@ def movie_details(request, pk):
 
     if request.method == 'PUT':
         movie = Movie.objects.get(pk=pk)
-        serializer = MovieSerializer(data=request.data)
+        serializer = MovieSerializer(movie, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
 
-
-    # if request.method == 'DELETE':
+    if request.method == 'DELETE':
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()
+        return Response()
